75b1a5bbb6e90d2b810d671f8392fcaf75136a45
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-04 11:23:28 -0500

    fixed the description

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a cute pomeranian dressed like a goat.', likes: 0 },
     { id: 2, path: 'images/pomer.jpeg', description: 'Photo of a cute pomeranian.', likes: 0 },
-    { id: 3, path: 'images/cutr.jpeg', description: 'Photo of a small pomeranian.', likes: 0 }
+    { id: 3, path: 'images/cutr.jpeg', description: 'Photo of a small even cuter pomeranian.', likes: 0 }
 
 
 ];
